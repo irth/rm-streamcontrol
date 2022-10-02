b18121b654e16c565fe384f51c5987182b1f5fff
Wojciech Kwolek <wojciech@kwolek.io>
2022-10-02 21:26:20 +0200

    add goobs library

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/irth/rm-streamcontrol
 
 go 1.18
+
+require (
+	github.com/andreykaipov/goobs v0.10.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/andreykaipov/goobs"
+)
 
 func main() {
 	fmt.Println("hello")
+	client, err := goobs.New("localhost:4455", goobs.WithPassword("secret"))
+	if err != nil {
+		panic(err)
+	}
+	v, err := client.General.GetVersion()
+	fmt.Printf("OBS version: %s\n", v.ObsVersion)
+	fmt.Printf("WS version: %s\n", v.ObsWebSocketVersion)
 }
